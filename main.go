24bdb5c79c7b4b98c76e4ead2a69e32e15e2d5d0
Thomas Obenaus <obenaus.thomas@gmail.com>
2018-10-23 11:53:05 +0200

    .Update first usage of the mapper

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/thomasobenaus/inframapper/mappedInfra"
+	"github.com/thomasobenaus/inframapper/tfstate"
+	"github.com/thomasobenaus/inframapper/trace"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	profile := "develop"
+	region := "eu-central-1"
+	tracerMapper := trace.New(os.Stdout)
+
+	keys := make([]string, 2)
+	keys[0] = "snapshot/base/networking/terraform.tfstate"
+	keys[1] = "snapshot/base/common/terraform.tfstate"
+	remoteCfg := tfstate.RemoteConfig{
+		BucketName: "741125603121-tfstate",
+		Keys:       keys,
+		Profile:    "shared",
+		Region:     "eu-central-1",
+	}
+
+	mappedInfra, err := mappedInfra.LoadAndMap(profile, region, remoteCfg, tracerMapper)
+
+	tracer := trace.NewInfo(os.Stdout)
+	if err != nil {
+		tracer.Info("Error loading/ mapping infrastructure: %s", err.Error())
+	}
+
+	var mappedResStr string
+	var unMappedAwsResStr string
+	for _, res := range mappedInfra.MappedResources() {
+		mappedResStr += "\t" + res.String() + "\n"
+	}
+	for _, res := range mappedInfra.UnMappedAwsResources() {
+		unMappedAwsResStr += "\t" + res.String() + "\n"
+	}
+
+	tracer.Info("Mapped Infra: ", mappedInfra)
+	tracer.Info("Mapped Resources [", len(mappedInfra.MappedResources()), "]:")
+	tracer.Info(mappedResStr)
+	tracer.Info("UnMapped AWS Resources [", len(mappedInfra.UnMappedAwsResources()), "]:")
+	tracer.Info(unMappedAwsResStr)
 }
